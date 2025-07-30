1954935e029ed4cf1c0c1d8f7b6153f246fd92c7
thrishaaaa <thrishakiran14@gmail.com>
2025-07-30 16:43:37 +0530

    implemented CRUD operations using PostMan

--- QuizApp/src/main/java/com/telusko/QuizApp/Controller/QuestionController.java
+++ QuizApp/src/main/java/com/telusko/QuizApp/Controller/QuestionController.java
@@ -3,9 +3,7 @@ package com.telusko.QuizApp.Controller;
 import com.telusko.QuizApp.Model.Question;
 import com.telusko.QuizApp.Service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,21 @@ public class QuestionController {
     public List<Question> getAllQuestions() {
 
         return questionService.getAllQuestions();
+    }
+
+    @GetMapping("/category/{category}")
+    public List<Question> getQuestionByCategory(@PathVariable String category) {
+        return questionService.getQuestionByCategory(category);
+    }
+
+    @PostMapping("/addQuestion")
+    public String addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
+    }
+
+    @DeleteMapping("/deleteQuestion/{id}")
+    public String deleteQuestion(@PathVariable int id) {
+        return questionService.deleteById(id);
+
     }
 }
--- QuizApp/src/main/java/com/telusko/QuizApp/Service/QuestionService.java
+++ QuizApp/src/main/java/com/telusko/QuizApp/Service/QuestionService.java
@@ -15,4 +15,18 @@ public class QuestionService {
     public List<Question> getAllQuestions() {
         return questionDao.findAll();
     }
+
+    public List<Question> getQuestionByCategory(String category) {
+        return questionDao.findByCategory(category);
+    }
+
+    public String addQuestion(Question question) {
+        questionDao.save(question);
+        return "Question added successfully";
+    }
+
+    public String deleteById(int id) {
+        questionDao.deleteById(id);
+        return "Question deleted successfully";
+    }
 }
